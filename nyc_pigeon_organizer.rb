98ba1d885016bad074299c4bf5332bbf24881870
Chaya Deaver <chayadeaver@gmail.com>
2019-08-13 15:39:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,63 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  # # pigeon_list = 
+  # # {
+  # #   "Theo" => {
+  # #   :color => ["purple", "grey"],
+  # #   :gender => ["male"],
+  # #   :lives => ["Subway"]
+  # #   },
+    
+  # #   "Peter Jr." => {
+  # #     :color => ["purple", "grey"],
+  # #     :gender => ["male"],
+  # #     :lives => ["Library"]
+  # #   },
+      
+  # #   "Lucky" => {
+  # #     :color => ["purple"],
+  # #     :gender => ["male"],
+  # #     :lives => ["Central Park"]
+  # #   },
+    
+  # #   "Ms. K" => {
+  # #     :color => ["grey", "white"],
+  # #     :gender => ["female"],
+  # #     :lives => ["Central Park"]
+  # #   },
+    
+  # #   "Queenie" => {
+  # #     :color => ["white", "brown"],
+  # #     :gender => ["female"],
+  # #     :lives => ["Subway"]
+  # #   },
+    
+  # #   "Andrew" => {
+  # #     :color => ["white"],
+  # #     :gender => ["male"],
+  # #     :lives => ["City Hall"]
+  # #   },
+    
+  # #   "Alex" => {
+  # #     :color => ["white", "brown"],
+  # #     :gender => ["male"],
+  # #     :lives => ["Central Park"]
+  # #   }
+    
+  # }
+  
+  new_pigeon = {}
+  data.each do |keys, value|
+    value.each do |colors, array|
+      array.each do |name|
+        # binding.pry
+      end
+    end
+  end
+  
+     new_pigeon[array[0]][:color] = value
+     new_pigeon[name][:gender] = value
+     new_
 end
+
